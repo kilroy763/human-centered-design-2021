965e2519c52fe7a165937dc3385de425fdf548a6
kilroy763 <58260542+kilroy763@users.noreply.github.com>
2021-04-23 13:35:20 +0200

    added nonsense

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 console.log('hi');
 
+audio =  document.getElementById("oneU2")
 
+
+window.addEventListener("keypress", (event) => { 
+    console.log(event)
+    if (event.key === "1"){
+        console.log("1")
+        audio.play();    
+        audio.volume = 0.05
+    } else{
+        audio.pause();    
+        
+    }
+    })
 
 
